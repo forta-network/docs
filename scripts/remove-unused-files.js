aa450816547b4162739dca155fd1d90f8ff45c54
Artem Kovalchuk <kovart.dev@gmail.com>
2024-05-28 15:34:54 +0300

    Add script to remove unused files

--- scripts/remove-unused-files.js
+++ scripts/remove-unused-files.js
@@ -1,81 +1,214 @@
-const { globSync } = require('glob')
-const yaml = require('yamljs')
 const fs = require('fs')
 const path = require('path')
+const yaml = require('yamljs')
+const { globSync } = require('glob')
+
+const SHOULD_REMOVE = false;
+const SHOULD_LOG = true;
 
 // Load and parse the mkdocs config
-const mkdocsConfig = yaml.load('mkdocs.yml')
-const docsDir = '../docs'
+const docsDir = path.resolve(__dirname, '../docs')
 
-function extractFilePaths(nav, fileList) {
+function exploreMarkdownFilesFromConfig (nav, markdownList, mediaList, visited) {
   if (Array.isArray(nav)) {
-    nav.forEach(item => extractFilePaths(item, fileList));
+    nav.forEach(item => exploreMarkdownFilesFromConfig(item, markdownList, mediaList, visited))
   } else if (typeof nav === 'object') {
     Object.values(nav).forEach(value => {
       if (typeof value === 'string') {
-        // Check if it's a local file (not a URL)
-        if (value.endsWith('.md') || value.endsWith('.html')) {
-          const fullPath = path.join(docsDir, value);
-          fileList.add(fullPath);
-        }
+        processFile(path.join(docsDir, value), markdownList, mediaList, visited)
       } else {
-        extractFilePaths(value, fileList);
-      }
-    });
-  }
-}
-
-const mkdocsFiles = new Set()
-extractFilePaths(mkdocsConfig.nav, mkdocsFiles)
-
-// List of markdown files from mkdocs.yml
-for (const page of mkdocsConfig.nav) {
-  if (typeof page === 'string') {
-    mkdocsFiles.add(page)
-  } else if (typeof page === 'object') {
-    Object.values(page).forEach((file) => {
-      if (typeof file === 'string') {
-        mkdocsFiles.add(file)
+        exploreMarkdownFilesFromConfig(value, markdownList, mediaList, visited)
       }
     })
   }
 }
 
-console.log(`Markdown files: ${mkdocsFiles.size}`)
-for (const file of mkdocsFiles) {
-  console.log('- ' + file)
+function exploreFilesFromFile (filePath, markdownLinks, mediaLinks, visited) {
+  let absolutePath = path.resolve(filePath)
+
+  if (visited.has(absolutePath)) {
+    return
+  }
+
+  visited.add(absolutePath)
+
+  if (absolutePath.includes('https:') || absolutePath.includes('http:')) {
+    return
+  }
+
+  let content
+  try {
+    content = fs.readFileSync(absolutePath, 'utf8')
+  } catch (error) {
+    console.error(`Error reading file ${absolutePath}: ${error}`)
+    return
+  }
+
+  const markdownLinkRegex = /\[.*?\]\((\S+\.[a-zA-Z-]+)(?:#[a-zA-Z-\d]+)?(?:\s+".+")?\s*\)/gi
+  const linkRegex = /docs\.forta\.network\/en\/latest\/([a-zA-Z-/\d\.]+)(?:#[a-zA-Z-\d]+)?/gi
+  const imgTagRegex = /<img.*\s+src="(.*?)"/gi
+  const videoTagRegex = /<video.*\s+src="(.*?)"/gi
+
+  let match
+  // Process markdown links
+  while ((match = markdownLinkRegex.exec(content)) !== null) {
+    processMarkdownLink(match[1], absolutePath, markdownLinks, mediaLinks, visited)
+  }
+
+  // Process site links
+  while ((match = linkRegex.exec(content)) !== null) {
+    processSiteLink(match[1], markdownLinks, mediaLinks, visited)
+  }
+
+  // folder/page-name.md -> folder/page-name/
+  const pagePath = getPagePath(absolutePath)
+
+  // Process HTML img tags
+  while ((match = imgTagRegex.exec(content)) !== null) {
+    processMediaLink(match[1], pagePath, markdownLinks, mediaLinks, visited)
+  }
+
+  // Process HTML video tags
+  while ((match = videoTagRegex.exec(content)) !== null) {
+    processMediaLink(match[1], pagePath, markdownLinks, mediaLinks, visited)
+  }
 }
 
-// Find all .md files in the project
-const allMarkdownFiles = globSync(`${docsDir}/**/*.md`)
-const notUsedMarkdownFiles = allMarkdownFiles.filter((file) => {
-  const relativeFile = path.relative(docsDir, file)
-  return !mkdocsFiles.has(relativeFile)
+// path/docs/readme-file.md => path/docs/readme-file/
+function getPagePath (absolutePath) {
+  const parsedPath = path.parse(absolutePath)
+
+  const folderPath = parsedPath.dir
+  const fileName = parsedPath.name // no ext
+  const pagePath = folderPath + '/' + fileName + '/'
+
+  return pagePath
+}
+
+function processFile (fullPath, markdownLinks, mediaLinks, visited) {
+  const fullPathLowerCase = fullPath.toLowerCase()
+
+  if (fullPathLowerCase.endsWith('.md')) {
+    markdownLinks.add(fullPath)
+    exploreFilesFromFile(fullPath, markdownLinks, mediaLinks, visited)
+  } else if (/\.(png|jpg|jpeg|gif|bmp|svg|mp4|avi|mov|webm|pdf|csv)$/i.test(fullPathLowerCase)) {
+    mediaLinks.add(fullPath)
+  }
+}
+
+function processSiteLink (linkPath, markdownLinks, mediaLinks, visited) {
+  if(!(/\S+\.\S+/.test(linkPath))) {
+    linkPath = linkPath.replace(/\/$/, "") + '.md';
+  }
+
+  let absolutePath = path.resolve(docsDir, linkPath)
+
+  processFile(absolutePath, markdownLinks, mediaLinks, visited)
+}
+
+function processMarkdownLink (link, currentPath, markdownLinks, mediaLinks, visited) {
+  const fullPath = path.resolve(path.dirname(currentPath), link)
+  processFile(fullPath, markdownLinks, mediaLinks, visited)
+}
+
+function processMediaLink (link, currentPath, markdownLinks, mediaLinks, visited) {
+  const fullPath = path.resolve(currentPath, link)
+  processFile(fullPath, markdownLinks, mediaLinks, visited)
+}
+
+// Here we start execution
+// ------------
+
+const EXCLUDE_FOLDERS = ['forta-api-reference', 'stylesheets', 'contracts'];
+const INCLUDE_DOC_FILES = ['scam-detector-bot.md', 'attack-detector-bot.md'];
+
+
+
+// const mkdocsConfig = yaml.load('../mkdocs.yml')
+const mkdocsConfig = yaml.load('mkdocs.yml')
+
+const usedMarkdownFileSet = new Set()
+const usedMediaFileSet = new Set()
+const visitedFileSet = new Set()
+
+for(const docFile of INCLUDE_DOC_FILES) {
+  processFile(path.resolve(docsDir, docFile), usedMarkdownFileSet, usedMediaFileSet, visitedFileSet)
+}
+
+// Parse mkdocs config
+exploreMarkdownFilesFromConfig(mkdocsConfig.nav, usedMarkdownFileSet, usedMediaFileSet, visitedFileSet)
+exploreMarkdownFilesFromConfig(mkdocsConfig.theme, usedMarkdownFileSet, usedMediaFileSet, visitedFileSet)
+// Parse meta tags
+exploreFilesFromFile(path.resolve(docsDir, '../overrides', 'main.html'), usedMarkdownFileSet, usedMediaFileSet, visitedFileSet)
+
+// Recursively find all related files referenced within each file
+for (const filePath of [...usedMarkdownFileSet]) {
+  exploreFilesFromFile(filePath, usedMarkdownFileSet, usedMediaFileSet, visitedFileSet)
+}
+
+const allMarkdownFiles = globSync(`${docsDir}/**/*.md`, { ignore: 'node_modules/**', absolute: true })
+const allMediaFiles = globSync(`${docsDir}/**/*.{png,jpg,jpeg,gif,bmp,svg,mp4,avi,mov,webm,pdf,csv}`, {
+  ignore: 'node_modules/**',
+  absolute: true
 })
 
-console.log(`-----------`)
+const canBeRemovedMarkdownFiles = new Set(allMarkdownFiles.filter(file => !usedMarkdownFileSet.has(file) && !file.includes('index.md')))
+const canBeRemovedMediaFiles = new Set(allMediaFiles.filter(file => !usedMediaFileSet.has(file)))
 
-console.log('Removing unused markdown files...')
-for (const file of notUsedMarkdownFiles) {
-  console.log('- ' + file)
-  fs.unlinkSync(file)
+for(const folder of EXCLUDE_FOLDERS) {
+  const folderPath = path.resolve(docsDir, folder)
+
+  for(const filePath of canBeRemovedMarkdownFiles) {
+    if(filePath.includes(folderPath)) {
+      canBeRemovedMarkdownFiles.delete(filePath)
+    }
+  }
+
+  for(const filePath of canBeRemovedMediaFiles) {
+    if(filePath.includes(folderPath)) {
+      canBeRemovedMediaFiles.delete(filePath)
+    }
+  }
 }
 
-// Find and remove unused media files
-const mediaExtensions = ['png', 'jpg', 'jpeg', 'gif', 'svg', 'mp4', 'avi', 'pdf', 'csv']
-const mediaFiles = globSync(`${docsDir}/**/*.{${mediaExtensions.join(',')}}`)
+console.log()
+console.log('-----------------------------')
+console.log(`Used markdown files: ${usedMarkdownFileSet.size}`)
+console.log(`Unused markdown files: ${canBeRemovedMarkdownFiles.size}`)
+console.log(`Used media files: ${usedMediaFileSet.size}`)
+console.log(`Unused media files: ${canBeRemovedMediaFiles.size}`)
+console.log('-----------------------------')
+console.log()
 
-console.log(`-----------`)
-console.log(`Media files: ${mediaFiles.length}`)
-console.log('Removing unused media files...')
-for (const mediaFile of mediaFiles) {
-  const isUsed = [...mkdocsFiles].some(file => {
-    const content = fs.readFileSync(file, 'utf-8')
-    return content.includes(path.basename(mediaFile))
-  })
+if(SHOULD_LOG) {
+  for (const file of usedMarkdownFileSet) {
+    console.log(`[IN USE]: ${file}`)
+  }
 
-  if (!isUsed) {
-    console.log('- ' + mediaFile)
-    fs.unlinkSync(mediaFile)
+  console.log('--------------------')
+
+  for (const file of canBeRemovedMarkdownFiles) {
+    console.log(`[REMOVE]: ${file}`)
+  }
+
+  console.log('--------------------')
+
+  for (const file of usedMediaFileSet) {
+    console.log(`[IN USE]: ${file}`)
+  }
+
+  console.log('--------------------')
+
+  for (const file of canBeRemovedMediaFiles) {
+    console.log(`[REMOVE]: ${file}`)
+  }
+}
+
+if(SHOULD_REMOVE) {
+  for (const file of canBeRemovedMarkdownFiles) {
+    fs.unlinkSync(file);
+  }
+  for (const file of canBeRemovedMediaFiles) {
+    fs.unlinkSync(file);
   }
 }
